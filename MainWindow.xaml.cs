a949c9c36cdac289012e069c7c958b0875a66c4d
zeroichi <zeroichi0@gmail.com>
2016-05-03 03:44:19 +0900

    認証とお気に入りの取得動作を試作
    add: PINコード入力ウィンドウを追加
    add: 認証とお気に入りの取得コードを追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LinqToTwitter;
+using System.Diagnostics;
 
 namespace TwitterFavoriteImagesCollector
 {
@@ -20,9 +22,87 @@ namespace TwitterFavoriteImagesCollector
     /// </summary>
     public partial class MainWindow : Window
     {
+        TwitterContext twitter_context = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// PINコード入力ウィンドウを表示し、ユーザが入力したPINコードを返す。
+        /// </summary>
+        /// <returns></returns>
+        private string GetPinCode()
+        {
+            string pin_code;
+            var pin_input_window = new PinInputWindow();
+
+            pin_input_window.ShowDialog();
+            pin_code = pin_input_window.textbox_pincode.Text;
+            Debug.WriteLine(pin_code);
+            return pin_code;
+        }
+
+        private async void button_authenticate_Click(object sender, RoutedEventArgs e)
+        {
+            var auth = new PinAuthorizer()
+            {
+                // アプリケーションを識別するための ConsumerKey と ConsumerSecret を指定する必要がある。
+                CredentialStore = new InMemoryCredentialStore
+                {
+                    // ConsumerKey = Environment.GetEnvironmentVariable(OAuthKeys.TwitterConsumerKey),
+                    // ConsumerSecret = Environment.GetEnvironmentVariable(OAuthKeys.TwitterConsumerSecret)
+                    ConsumerKey = "8P3QaBrEeizpG3jjcTir9g",
+                    ConsumerSecret = "qVSl0rgFzxDjpQkvzZptuZ5qaTWb7aGJZguFK1Pk4"
+                },
+                GoToTwitterAuthorization = pageLink =>
+                {
+                    Debug.WriteLine(pageLink);
+                    Process.Start(pageLink);
+                },
+                GetPin = () =>
+                {
+                    string pin_code = "";
+                    // ウィンドウの表示は UI スレッド上で行われる必要があるので、Dispatcherを使用する。
+                    // 参考: http://blogs.wankuma.com/naka/archive/2009/02/12/168020.aspx
+                    var dispatcher = Application.Current.Dispatcher;
+                    if (dispatcher.CheckAccess())
+                    {
+                        pin_code = GetPinCode();
+                    }
+                    else
+                    {
+                        pin_code = dispatcher.Invoke<string>(GetPinCode);
+                    }
+                    return pin_code;
+                }
+            };
+            try
+            {
+                await auth.AuthorizeAsync();
+                twitter_context = new TwitterContext(auth);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private async void button_get_Click(object sender, RoutedEventArgs e)
+        {
+            if (twitter_context == null)
+            {
+                MessageBox.Show("最初に認証してください。");
+                return;
+            }
+            var favorites = await (from fav in twitter_context.Favorites where fav.Type == FavoritesType.Favorites && fav.Count == 200 select fav).ToListAsync();
+            var sb = new StringBuilder();
+            foreach(var f in favorites)
+            {
+                sb.AppendLine(f.Text);
+            }
+            textbox_result.Text = sb.ToString();
         }
     }
 }
